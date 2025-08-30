f8845b2ff341ec6a830f73e25427264dadeeb3fd
Evanio Tech <evanioshark@gmail.com>
2025-08-30 14:31:09 -0300

    teste

--- ToonCards/Tooncards/App.js
+++ ToonCards/Tooncards/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your teste!</Text>
+      <Text>Este é um teste!</Text>
       <StatusBar style="auto" />
     </View>
   );
