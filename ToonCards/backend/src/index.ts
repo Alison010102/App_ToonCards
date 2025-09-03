9d713067f0afb5f864318f686244799b82ffc593
Alison010102 <secundo2002@gmail.com>
2025-09-03 18:17:14 -0300

    finalizado o CRUD e implementado no sing

--- ToonCards/backend/src/index.ts
+++ ToonCards/backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import router from "express";
+import router from "./Routes/UserRoutes";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3333,()=>{
-    console.log("Servidor rodando na porta 3333")
-})
+app.use("/api", router);
+
+app.listen(3333, () => {
+  console.log("Servidor rodando na porta 3333");
+});
---
+++ ToonCards/src/styles/colors.js
@@ -0,0 +1,11 @@
+export const Colors = {
+    corMaisEscura: '#1C1B1F',
+    segundaMaisEscura: '#27292C',
+    amareloQueimado: '#E7BD52',
+    amareloMaisClaro: '#ffd4693f',
+    cinzaClaro: '#B6BDB6',
+    cinzaEscuro: '#A1ADAF',
+    corBranca: '#ffffffff',
+    corBrancaMaisEscura: '#f6f4f4ff',
+    corPreta: '#000000'
+}
