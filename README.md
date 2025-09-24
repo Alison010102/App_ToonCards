06d92417902ecba4029096e25c14805f758c020f
EvanioTech <evanioshark@gmail.com>
2025-09-24 19:49:32 -0300

    ajuste no login

--- README.md
+++
@@ -1,5 +0,0 @@
-##Anotações Alison{
-foi terminado o CRUD e adicionado na implementação do SING(03/09/25)
-
-falta realizar: criar o front de cadastro e iniciar a pasta home
-}
--- ToonCards/Tooncards/src/components/home/homeStyle.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";  
-import { Colors } from "../../styles/colors";
-
-export const HomeStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.cinzaEscuro,
-    }
-})
